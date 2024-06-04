bdaf8cc3dbf0ac30afee2cc6551f7af901ae58a6
DerrickDevelops <derrickdevelops@gmail.com>
2024-06-03 20:50:17 -0400

    Added Content Folder
    Added Content Folder to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar.jsx';
 import Header from './Header.jsx';
+import Content from './Content.jsx'
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Content />
     </>
   );
 }
---
+++ src/Content.jsx
@@ -0,0 +1,9 @@
+const Content = () => {
+    return(
+        <nav>
+            <h3>Hello Again</h3>
+        </nav>
+    )
+}
+
+export default Content
