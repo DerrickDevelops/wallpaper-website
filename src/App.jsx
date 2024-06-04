d57a4db4418b48a78b101a73d835566ff8c3d387
DerrickDevelops <derrickdevelops@gmail.com>
2024-06-03 20:47:36 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar.jsx';
+import Header from './Header.jsx';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <nav>
+            <h2>Hey There</h2>
+        </nav>
+    )
+}
+
+export default Header
