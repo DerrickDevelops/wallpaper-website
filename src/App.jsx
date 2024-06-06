9bd2012b477dd2c3f343908cc72f448c9305fa9a
DerrickDevelops <derrickdevelops@gmail.com>
2024-06-05 22:42:54 -0400

    Updated Content
    Added Layout CSS & Added removed Content File

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar.jsx';
 import Header from './Header.jsx';
-import Content from './Content.jsx';
+
 import Footer from './Footer.jsx';
+import './layout.css';
 
 function App() {
   return (
-    <>
+    <div className="main-container">
       <Navbar />
       <Header />
-      <Content />
       <Footer />
-    </>
+    </div>
   );
 }
 
