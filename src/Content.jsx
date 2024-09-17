b79b0acdf7081c590444b554e653dcedc63dcb26
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-09-16 22:33:33 -0400

    Update Content.jsx
    Added New section "New Releases" to wallpaper container

--- src/Content.jsx
+++ src/Content.jsx
@@ -9,6 +9,13 @@ const Content = () => {
 
     return(
         <main>
+            <h3>New Releases</h3>
+            <section className="new-releases">
+            <a href="" alt=""></a>
+            <a href="" alt=""></a>
+            <a href="" alt=""></a>
+            <a href="" alt=""></a>
+            </section>
             <h3>Featured Wallpapers</h3>
             <section className="wallpaper-container">
             <a href="" alt=""></a>
