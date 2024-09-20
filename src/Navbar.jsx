c62e4f77351dc60964b1fb27e22e15239e71f15e
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-09-19 23:40:51 -0400

    Update Navbar.jsx
    Title change

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@
 const Navbar = () => {
     return(
         <nav>
-            <a href=''>New Release</a>
+            <a href=''>New Releases</a>
             <a href=''>Featured</a>
             <a href=''>Contact</a>
         </nav>
