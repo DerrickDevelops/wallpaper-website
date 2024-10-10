50e4ae1bbca1f62621b1c280231cc36e6a3a89b2
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-10-09 23:30:23 -0400

    Update Content.jsx
    Changed main header text & added image tags inside of new releases div

--- src/Content.jsx
+++ src/Content.jsx
@@ -9,13 +9,13 @@ const Content = () => {
 
     return(
         <main>
-            <h2>Collection</h2>
+            <h2>The Collection</h2>
             <h3>New Releases</h3>
             <section className="new-release-container">
-            <a href="" alt=""></a>
-            <a href="" alt=""></a>
-            <a href="" alt=""></a>
-            <a href="" alt=""></a>
+            <a href=""><img src='' alt=""/></a>
+            <a href=""><img src='' alt=""/></a>
+            <a href=""><img src='' alt=""/></a>
+            <a href=""><img src='' alt=""/></a>
             </section>
             <h3>Featured Wallpapers</h3>
             <section className="Featured-container">
