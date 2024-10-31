2aa4de9be757a8658f06a447ef2c80067232e66c
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-10-30 23:23:43 -0400

    Update Header.jsx
    Renamed wallpaper-backdrop div & added new div inside.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,17 @@
 const Header = () => {
+    
+    
+    
+    
+    
     return(
         <header>
             <h2>Wallpaper's</h2>
             <p>By Derrick</p>
-            <div className="wallpaper-backdrop">
-                
+            <div className="wallpaper-backdrop-container">
+                <div className="wallpapers">
+                    
+                </div>
             </div>
         </header>
     )
