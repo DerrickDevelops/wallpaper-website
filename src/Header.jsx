f1f05e18b83c1aa957b44ca09be15ce1f88e8e11
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-11-05 22:10:39 -0500

    Update Header.jsx
    Updated h2 element to h1 and changed header text.

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
     
     return(
         <header>
-            <h2>Wallpaper's</h2>
-            <p>By Derrick</p>
+            <h1>Derrick Develops Wallpapers</h1>
+            <p>Designed for everyone</p>
             <div className="wallpaper-backdrop-container">
                 <div className="wallpapers">
                     
