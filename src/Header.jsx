134c659f4900c46692fc51419070d1c4db600516
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-11-11 23:29:34 -0500

    Update Header.jsx
    Changed Header and sub header text

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
     
     return(
         <header>
-            <h1>Derrick Develops Wallpapers</h1>
-            <p>Designed for everyone</p>
+            <h1>Wallpapers</h1>
+            <p>Designed by Derrick.</p>
             <div className="wallpaper-backdrop-container">
                 <div className="wallpapers">
                     
