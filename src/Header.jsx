bba4e2d5421c1183a7cab2c4d6c73a12959fc220
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-11-11 23:30:48 -0500

    Update Header.jsx
    Added class name for sub header p element.

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return(
         <header>
             <h1>Wallpapers</h1>
-            <p>Designed by Derrick.</p>
+            <p className='subheader'>Designed by Derrick.</p>
             <div className="wallpaper-backdrop-container">
                 <div className="wallpapers">
                     
